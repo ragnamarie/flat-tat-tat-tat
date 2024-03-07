05ffe46ca95664837d0e4d76b58edacefefda3c8
ragnamarie <ragna.steinboemer@gmail.com>
2024-03-07 13:36:53 +0100

    remounting issue fixed

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
 import Counter from "../Components/Counter";
 import Link from "next/link";
 import useSWR from "swr";
-import GlobalStyle from "../styles";
-import Layout from "../Components/Layout";
 
 export default function HomePage() {
-  const fetcher = (url) => fetch(url).then((response) => response.json());
-
-  const { data: emptyFlatsData, isLoading: loadingEmptyFlatsData } = useSWR(
-    "/api/emptyFlats",
-    fetcher
-  );
+  const { data: emptyFlatsData, isLoading: loadingEmptyFlatsData } =
+    useSWR("/api/emptyFlats");
 
   if (loadingEmptyFlatsData) {
     return <h1>LOADING...</h1>;
@@ -23,14 +17,13 @@ export default function HomePage() {
   console.log(emptyFlatsData);
 
   return (
-    <Layout>
-      <GlobalStyle />
+    <>
       <h1>
         <Counter emptyFlats={emptyFlatsData} />
       </h1>
       <h3>
         <Link href="/report">Report an empty flat</Link>
       </h3>
-    </Layout>
+    </>
   );
 }
