9e5dacc97ad1ec68378fca75cb5cae1b63daf20d
ragnamarie <ragna.steinboemer@gmail.com>
2024-03-07 14:48:46 +0100

    added dummy flat component

--- pages/tickets.js
+++ pages/tickets.js
@@ -1,3 +1,25 @@
+import Link from "next/link";
+import useSWR from "swr";
+import ReportedFlats from "../Components/ReportedFlats";
+
 export default function TicketPage() {
-  return;
+  const { data: emptyFlatsData, isLoading: loadingEmptyFlatsData } =
+    useSWR("/api/emptyFlats");
+
+  if (loadingEmptyFlatsData) {
+    return <h1>LOADING...</h1>;
+  }
+
+  if (!emptyFlatsData) {
+    return;
+  }
+
+  console.log(emptyFlatsData);
+
+  return (
+    <>
+      <h1>REPORTED FLATS</h1>
+      <ReportedFlats emptyFlats={emptyFlatsData} />
+    </>
+  );
 }
