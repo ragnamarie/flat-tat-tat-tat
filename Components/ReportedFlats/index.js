de75ae1e477d7c5519dfb6e2f6f2d673198a6795
ragnamarie <ragna.steinboemer@gmail.com>
2024-03-07 16:42:26 +0100

    added ticket stuff

--- Components/ReportedFlats/index.js
+++ Components/ReportedFlats/index.js
@@ -1,6 +1,37 @@
 import styled from "styled-components";
+import Link from "next/link";
+
+export const StyledList = styled.ul`
+  display: grid;
+  gap: 40px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+export const StyledTicket = styled.span`
+  display: inline-block;
+  position: relative;
+  font-family: Helvetica;
+  font-weight: 700;
+  border: solid white;
+  padding: 10px;
+  background-color: #2422d5;
+  color: white;
+  min-width: 200px;
+`;
 
 export default function ReportedFlats({ emptyFlats }) {
   console.log(emptyFlats);
-  return <></>;
+  return (
+    <StyledList>
+      {emptyFlats.map((flat, index) => (
+        <li key={index}>
+          <StyledTicket>
+            <Link href="/">{flat.street}</Link>
+          </StyledTicket>
+        </li>
+      ))}
+    </StyledList>
+  );
 }
